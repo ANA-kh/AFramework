53dd04e176ac58ee4f37a637004b1ee511f967bd
ANA_h <1154073432@qq.com>
2024-01-23 00:30:32 +0800

    添加模拟器加载

---
+++ Assets/Scripts/AFramework/ResModule/SimulationResources/SimulationRes.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using UnityEditor;
+using UnityEngine;
+
+namespace AFramework.ResModule.SimulationResources
+{
+    public class SimulationRes : Res
+    {
+        private static string AssetRoot = "Assets/LoxodonFramework/BundleExamples";
+        public SimulationRes(string path, IResManager resManager) : base(path, resManager) { }
+        public override IRes Load()
+        {
+            _result = AssetDatabase.LoadAssetAtPath(GetFullPath(), typeof(Object));
+            if (_result == null)
+            {
+                Debug.LogException(new System.Exception($"Load asset failure.The asset named \"{GetFullPath()}\" is not found."));
+            }
+            
+            OnFinish();
+            return this;
+        }
+
+        private string GetFullPath()
+        {
+            const string target = "BundleExamples";
+            var index = _path.IndexOf(target);
+            if (index < 0)
+            {
+                Debug.LogError("路径匹配index<0");
+                index = 0;
+            }
+            else
+            {
+                index += target.Length;
+            }
+
+            var relativePath = _path.Substring(index);
+            return System.IO.Path.Combine(AssetRoot, relativePath.TrimStart('/'));
+        }
+
+        protected override IEnumerator CoLoad()
+        { 
+            _result = AssetDatabase.LoadAssetAtPath(GetFullPath(), typeof(Object));
+            if (_result == null)
+            {
+                Debug.LogException(new System.Exception($"Load asset failure.The asset named \"{GetFullPath()}\" is not found."));
+            }
+            OnFinish();
+            yield break;
+        }
+    }
+}
---
+++ Assets/Scripts/AFramework/ResModule/SimulationResources/SimulationResManager.cs
@@ -0,0 +1,14 @@
+namespace AFramework.ResModule.SimulationResources
+{
+    public class SimulationResManager : ResManager
+    {
+        protected override Res GetOrCreateRes(string path)
+        {
+            if (_resMap.TryGetValue(path, out var res))
+                return res;
+            res = new SimulationRes(path, this);
+            Retain(res);
+            return res;
+        }
+    }
+}
